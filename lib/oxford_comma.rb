3a8b07d4cfd96e367892e25697fd630edf1d8bb6
Lorena <lorena.giddens@outlook.com>
2018-06-05 13:03:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+  # convert into string using the Oxford comma
+arraySize = array.size
 
+case arraySize
+  when 1
+    return array.join
+  when 2
+    Array.new = array.join(" and ")
+    return Array.new
+  else
+    array.last = " and " << array.last
+    Array.new = array.join(", ")
+    return Array.new
+  end
 end
+  
