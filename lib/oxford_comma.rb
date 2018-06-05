eca60d043f56a26bafb0cc6aa2a26153e2427f0d
Lorena <lorena.giddens@outlook.com>
2018-06-05 13:08:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+  # convert into string using the Oxford comma
+arraySize = array.size
+final_result = Array.new
 
+case arraySize
+  when 1
+    return array.join
+  when 2
+    final_result = array.join(" and ")
+    return final_result
+  else
+    array[-1] = "and " << array[-1]
+    final_result = array.join(", ")
+    return final_result
+  end
 end
